a121700b3b598bae07af6bdf54b2f37f05085b37
Scottie Lee <scottie.c.f.lee@gmail.com>
2022-10-08 18:41:33 +0200

    added home page

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+import os, random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    y_data = txt_to_list("data/singlet-trace-1.txt")
+    return render_template("index.html", title="Home")
+
+@app.route("/challenge")
+def challenge():
+    qubit = random.choice(os.listdir("data/"))
+    y_data = txt_to_list("data/" + qubit)
     x_data = [item for item in range(0, len(y_data))]
-    return render_template("index.html", y_values=y_data, x_values=x_data, title="Home")
+    answer = good_answer(qubit)
+    return render_template("challenge.html", y_values=y_data, x_values=x_data, title="Challenge", answer=answer)
 
 def txt_to_list(txt_file):
     my_file = open(txt_file, "r")
@@ -16,6 +23,13 @@ def txt_to_list(txt_file):
     content_list.remove("")
     float_list = [float(i) for i in content_list]
     return float_list
+
+def good_answer(qubit):
+    answer = qubit.split("-")
+    if answer == "singlet":
+        return "zero"
+    else:
+        return "one"
 
 if __name__ == "__main__":
     app.run()
